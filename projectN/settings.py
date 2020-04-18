c8081f6d1eed426db78fa6164eb787955ca74035
noriwosaru <noriwosaru@gmail.com>
2020-04-18 18:42:34 +0700

    to solve conflict

--- projectN/settings.py
+++ projectN/settings.py
@@ -114,11 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-LOGIN_REDIRECT_URL = '/'
